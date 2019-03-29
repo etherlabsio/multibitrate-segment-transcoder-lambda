bab5f25d465b0944eda7902f46eb44399e3ec984
Lijo Jose <lijo@etherlabs.io>
2019-03-29 09:52:35 +0530

    updated ffmpeg package path and hls module

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@
 
 build:
 	env GOOS=linux go build -ldflags="-s -w" -o bin/hls-multirate-transcoder cmd/hls-multirate-transcoder/main.go
-	cd bin && wget https://github.com/etherlabsio/FFmpeg/releases/download/n4.0.2/ffmpeg-4.0.2-amazonlinux.tgz -O ffmpeg.tgz && tar -xzf ffmpeg.tgz && rm ffmpeg.tgz && cd -
-	wget https://github.com/etherlabsio/FFmpeg/releases/download/n4.0.2/shared-libs-amazonlinux.tgz -O libs.tgz && tar -xzf libs.tgz && rm libs.tgz
+	cd bin && wget https://s3.amazonaws.com/io.etherlabs.artifacts/shared/ffmpeg-4.0.2-amazonlinux.tgz -O ffmpeg.tgz && tar -xzf ffmpeg.tgz && rm ffmpeg.tgz && cd -
+	wget https://s3.amazonaws.com/io.etherlabs.artifacts/shared/ffmpeg-libs-amazonlinux.tgz -O libs.tgz && tar -xzf libs.tgz && rm libs.tgz
 	cp scripts/ffmpegexec.sh bin/
 clean:
 	rm -rf ./bin/*
---
+++ test/lambda_invoke_test.go
@@ -0,0 +1,69 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/etherlabsio/hls/pkg/hls"
+)
+
+func Test_lambdaInvoke(t *testing.T) {
+	tests := []struct {
+		name                    string
+		Bucket                  string
+		KeyPattern              string
+		start                   int
+		stop                    int
+		DRMKey                  []byte
+		DRMInitializationVector string
+		wantErr                 bool
+	}{
+		{
+			"Basic functionality",
+			"io.etherlabs.test",
+			"recordings/hls-test/out%04d.ts",
+			5,
+			9,
+			[]byte{122, 108, 181, 101, 231, 152, 205, 196, 127, 105, 169, 155, 109, 124, 120, 213},
+			"51532a902859d8b493b6db31a1d1cfc5",
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			// Create Lambda service client
+			sess := session.Must(session.NewSessionWithOptions(session.Options{
+				SharedConfigState: session.SharedConfigEnable,
+			}))
+
+			client := lambda.New(sess, &aws.Config{})
+
+			for k := tt.start; k <= tt.stop; k++ {
+				key := fmt.Sprintf(tt.KeyPattern, k)
+				request := hls.TranscodeEvent{tt.Bucket, key,
+					tt.DRMKey, tt.DRMInitializationVector, []string{"720p", "360p", "480p"}}
+
+				payload, err := json.Marshal(request)
+
+				if (err != nil) != tt.wantErr {
+					fmt.Println("Error marshalling testBasicLambdaFunctionality request")
+					return
+				}
+
+				_, err = client.Invoke(&lambda.InvokeInput{FunctionName: aws.String("ether-hls-multirate-transcoder"), Payload: payload, InvocationType: aws.String("Event")})
+
+				if (err != nil) != tt.wantErr {
+					fmt.Println("Error calling MyGetItemsFuntestBasicLambdaFunctionalityction")
+					return
+				}
+				//time.Sleep(2 * time.Second)
+			}
+		})
+	}
+}
